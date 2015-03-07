de5e78d7a7c9a315893551d1ebec7d10782f9804
Jono Xia <jono@fastmail.fm>
2015-03-06 23:40:25 -0800

    Added code file for generating plot 4

--- plot4.R
+++ plot4.R
@@ -1,4 +1,28 @@
 power <- read.table("just_two_days.txt", sep = ";", header = TRUE, na.strings = "?")
 
-power$Datetime <- strptime( paste(power$Date, power$Time, sep=" "),
+power$datetime <- strptime( paste(power$Date, power$Time, sep=" "),
                             "%d/%m/%Y %H:%M:%S")
+
+par(mfcol = c(2, 2))
+with(power, {
+
+    plot(datetime, Global_active_power, type="n", xlab="", ylab="Global Active Power")
+    lines(datetime, Global_active_power)
+
+    plot(datetime, Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
+    lines(datetime, Sub_metering_1, col="black")
+    lines(datetime, Sub_metering_2, col="red")
+    lines(datetime, Sub_metering_3, col="blue")
+    legend("topright",
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           lty=c(1,1,1),
+           col=c("black", "red", "blue")
+    )
+
+
+    plot(datetime, Voltage, type="n")
+    lines(datetime, Voltage)
+
+    plot(datetime, Global_reactive_power, type="n")
+    lines(datetime, Global_reactive_power)
+})
